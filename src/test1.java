5e6cb9cf86688eca4d8be6e8c13628d231bbd292
msozkilic <msozkilic@gmail.com>
2020-11-25 14:51:52 +0100

    version 1

---
+++ src/test1.java
@@ -0,0 +1,7 @@
+public class test1 {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println(".gitignore dosyasina gitmeyecek dosyalari yazacagiz");
+    }
+}
